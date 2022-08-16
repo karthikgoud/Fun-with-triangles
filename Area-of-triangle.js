3adb6e4f5e80704483abe7b98d5da5665d58acda
karthikgoud <karthikrv09@gmail.com>
2022-08-16 12:28:10 +0530

    feature: area negative input error

--- Area-of-triangle.js
+++ Area-of-triangle.js
@@ -5,15 +5,19 @@ const outputEl = document.querySelector("#output");
 calculateBtn.addEventListener("click", calculateArea);
 
 function calculateArea() {
-  const base = Number(input[0].value);
-  const height = Number(input[1].value);
+  if (input[0].value >= 0 && input[1].value >= 0) {
+    const base = Number(input[0].value);
+    const height = Number(input[1].value);
 
-  console.log(base);
-  console.log(height);
+    console.log(base);
+    console.log(height);
 
-  const areaOfTriangle = 0.5 * base * height;
+    const areaOfTriangle = 0.5 * base * height;
 
-  outputEl.innerText = `Area of Triangle is ${areaOfTriangle.toFixed(
-    2
-  )} (square units)`;
+    outputEl.innerText = `Area of Triangle is: ${areaOfTriangle.toFixed(
+      2
+    )} (square units)`;
+  } else {
+    outputEl.textContent = "Invalid input: Sides cannot have negative value.";
+  }
 }
